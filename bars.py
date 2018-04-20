a9371f98772fdd52268c6ecefff1175c2334c13b
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-04-20 20:45:15 +0300

    Added getting of information about smallest and biggest bars

--- bars.py
+++ bars.py
@@ -9,12 +9,16 @@ def load_json_data(filepath):
         return json.load(file)
 
 
-def get_biggest_bar(data):
-    pass
+def get_seats_count_in_bar(bar):
+    return bar['properties']['Attributes']['SeatsCount']
 
 
-def get_smallest_bar(data):
-    pass
+def get_biggest_bar(list_bars):
+    return max(list_bars, key=get_seats_count_in_bar)
+
+
+def get_smallest_bar(list_bars):
+    return min(list_bars, key=get_seats_count_in_bar)
 
 
 def get_closest_bar(data, longitude, latitude):
