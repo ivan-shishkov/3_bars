05cde4cd518e2f11d836f9afa30bf3c4ec31691f
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-04-20 21:19:51 +0300

    Added getting of information about nearest bar

--- bars.py
+++ bars.py
@@ -21,8 +21,22 @@ def get_smallest_bar(list_bars):
     return min(list_bars, key=get_seats_count_in_bar)
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def calculate_distance_to_bar(longitude, latitude):
+    def calculate(bar):
+        bar_coordinates = bar['geometry']['coordinates']
+        bar_longitude = bar_coordinates[0]
+        bar_latitude = bar_coordinates[1]
+
+        return ((bar_longitude - longitude) ** 2 + (
+                    bar_latitude - latitude) ** 2) ** 0.5
+
+    return calculate
+
+
+def get_nearest_bar(list_bars, longitude, latitude):
+    get_distance_to_bar = calculate_distance_to_bar(longitude, latitude)
+
+    return min(list_bars, key=get_distance_to_bar)
 
 
 if __name__ == '__main__':
